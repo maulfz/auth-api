892c05c2231d4de3a7f023ede2042120ff487cfa
maulabdlh <maulana.abdillah@wingscorp.com>
2024-10-09 14:06:24 +0700

    feat(homepage): add default page test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -33,6 +33,22 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
+  describe('when GET /', () => {
+    it('should return 200 and say hello', async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello itWorks!');
+    });
+  });
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
